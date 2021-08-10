04c3e355207e3b5df1be56984acaa27edd01d98d
guayana <isabelaguayanaz@gmail.com>
2021-08-09 21:45:22 -0300

    pagina de post leitura

--- src/pages/posts/[slug].tsx
+++ src/pages/posts/[slug].tsx
@@ -3,6 +3,7 @@ import { getSession } from "next-auth/client"
 import Head from "next/head"
 import { RichText } from "prismic-dom"
 import { getPrismicClient } from "../../services/prismic"
+import styles from './post.module.scss'
 
 interface PostProps {
     post: {
@@ -20,11 +21,14 @@ export default function Post({ post }: PostProps){
                 <title>{post.title} | Ignews </title>
             </Head>
 
-            <main>
-                <article>
+            <main className={styles.container}>
+                <article className={styles.post}>
                     <h1>{post.title}</h1>
                     <time>{post.updatedAt}</time>
-                    <div  dangerouslySetInnerHTML={{ __html: post.content }} />
+                    <div
+                        className={styles.postContent}
+                        dangerouslySetInnerHTML={{ __html: post.content }} 
+                    />
                 </article>
             </main>
         </>
