ee5b2cc4918967b2aa493340fbebe67cc72e81af
guayana <isabelaguayanaz@gmail.com>
2022-01-24 18:03:07 -0300

    arrumando os testes para funcionar

---
+++ src/components/ActiveLink/ActiveLink.spec.js
@@ -0,0 +1,24 @@
+import { render } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import { ActiveLink } from './index'
+
+
+jest.mock('next/router', () => {
+  return {
+    useRouter() {
+      return {
+        asPath: '/'
+      }
+    }
+  }
+})
+
+test('active link renders correctly', () => {
+  const { getByText } = render(
+    <ActiveLink href="/" activeClassName="active">
+      <a>Home</a>
+    </ActiveLink>
+  )
+
+  expect(getByText('Home')).toBeInTheDocument()
+})
